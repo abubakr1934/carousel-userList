c24771c36e34d265cc0d7fc1d841162dc4b41915
Abubakr Khanooni <abubakrkhanooni20@gmail.com>
2024-06-26 17:01:02 +0530

    ff

--- userList-carousel/src/App.jsx
+++ userList-carousel/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 import CarouselComponent from './component/CarouselComponent'
 import Faq from './component/Faq'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import SignupLogin from './component/SignupLogin'
 import Homepage from './component/Homepage'
+import Feature from './component/Feature'
 function App() {
   return (
     <>
@@ -14,6 +15,7 @@ function App() {
       <Faq></Faq>
       {/* <SignupLogin></SignupLogin>  */}
       <Homepage></Homepage>
+      
       {/* <SignupLogin></SignupLogin> */}
     </>
   )
