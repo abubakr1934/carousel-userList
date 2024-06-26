936e544b3de37763f1f14f2d30abaad528dbe031
Abubakr Khanooni <abubakrkhanooni20@gmail.com>
2024-06-26 17:00:42 +0530

    f

---
+++ feature/feature/src/components/Feature.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Feature.css'
+const Feature = () => {
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="image-wrapper">
+          <img
+            alt="traveller"
+            src="https://images.unsplash.com/photo-1503457574462-bd27054394c1?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTg4MTkwMzB8&ixlib=rb-4.0.3&q=85"
+          />
+        </div>
+        <div className="content">
+          <h2>Card One</h2>
+          <p>
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="image-wrapper">
+          <img
+            alt="traveller"
+            src="https://images.unsplash.com/photo-1628152184584-1bec8d3a3cbe?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTg4MTk2MTJ8&ixlib=rb-4.0.3&q=85"
+          />
+        </div>
+        <div className="content">
+          <h2>Card Two</h2>
+          <p>
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="image-wrapper">
+          <img
+            alt="traveller"
+            src="https://images.unsplash.com/photo-1531210194434-2bc4d24185a8?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTg4MTk2MTJ8&ixlib=rb-4.0.3&q=85"
+          />
+        </div>
+        <div className="content">
+          <h2>Card Three</h2>
+          <p>
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Feature;
