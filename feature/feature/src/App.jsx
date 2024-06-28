1ba0872374f1cc67f49d14ae595d2cf348bca0d8
Abubakr Khanooni <abubakrkhanooni20@gmail.com>
2024-06-28 16:58:48 +0530

    friday

--- feature/feature/src/App.jsx
+++ feature/feature/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Feature from './components/Feature'
+// import Framer from './components/Framer'
 function App() {
 
   return (
     <>
     <Feature></Feature>
-      
+      {/* <Framer></Framer> */}
     </>
   )
 }
--- feature/feature/src/components/Feature.jsx
+++ feature/feature/src/components/Feature.jsx
@@ -7,11 +7,12 @@ const Feature = () => {
         <div className="image-wrapper">
           <img
             alt="traveller"
-            src="https://images.unsplash.com/photo-1503457574462-bd27054394c1?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTg4MTkwMzB8&ixlib=rb-4.0.3&q=85"
+            src="https://static.vecteezy.com/system/resources/thumbnails/002/406/611/small_2x/business-man-cartoon-character-vector.jpg"
           />
+          <h2 style={{color:"white"}} >Card One</h2>
         </div>
         <div className="content">
-          <h2>Card One</h2>
+          
           <p>
             Lorem Ipsum has been the industry's standard dummy text ever since
             the 1500s, when an unknown printer took a galley of type and
@@ -23,11 +24,13 @@ const Feature = () => {
         <div className="image-wrapper">
           <img
             alt="traveller"
-            src="https://images.unsplash.com/photo-1628152184584-1bec8d3a3cbe?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTg4MTk2MTJ8&ixlib=rb-4.0.3&q=85"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJGlG1l-7zwLb2IV5fJzh1GnKXOyC6kW7jG7r25yZ7IfkAx8F2L4qlVoFDzGcW59YNyvw&usqp=CAU"
           />
+          <h2 style={{color:"white"}}>Card Two</h2>
         </div>
+        
         <div className="content">
-          <h2>Card Two</h2>
+          
           <p>
             Lorem Ipsum has been the industry's standard dummy text ever since
             the 1500s, when an unknown printer took a galley of type and
@@ -39,11 +42,47 @@ const Feature = () => {
         <div className="image-wrapper">
           <img
             alt="traveller"
-            src="https://images.unsplash.com/photo-1531210194434-2bc4d24185a8?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTg4MTk2MTJ8&ixlib=rb-4.0.3&q=85"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT22TbgB_oZ5-aUm7oXw2tDQF0ZNYYhhlqbktZqIXWvHjxC3aN4HvUs57bz4UMns5U0QE&usqp=CAU"
           />
+          <h2 style={{color:"white"}}>Card Three</h2>
         </div>
         <div className="content">
-          <h2>Card Three</h2>
+          
+          <p>
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="image-wrapper">
+          <img
+            alt="traveller"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNblQWRB1Q5yrG_4xz0_Vry0MlmChiNRy9krDxj2QAMZ_pUQH8Xyb7sGPCXu_AlUtu_rc&usqp=CAU"
+          />
+          <h2 style={{color:"white"}}>Card Four</h2>
+        </div>
+        
+        <div className="content">
+          
+          <p>
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="image-wrapper">
+          <img
+            alt="traveller"
+            src="https://previews.123rf.com/images/jemastock/jemastock1709/jemastock170909479/85576396-young-man-cartoon-icon-vector-illustration-graphic-design.jpg"
+          />
+          <h2 style={{color:"white"}}>Card Five</h2>
+        </div>
+        <div className="content">
+          
           <p>
             Lorem Ipsum has been the industry's standard dummy text ever since
             the 1500s, when an unknown printer took a galley of type and
